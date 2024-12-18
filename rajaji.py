b873c0d8e696c9efe530066aeeb3bf637d4f3fe7
Hejwkwkwkk <pulkittravis@gmail.com>
2024-12-19 01:41:10 +0530

    Add files via upload

--- rajaji.py
+++ rajaji.py
@@ -1,10 +1,14 @@
+# Don't use quotes( " and ' )
+#SCRIPT BY TU ADMIN NHI HAI TU CHUTIYA HAI
+  
+#Enter Your Bot Token here get it from @botfarher
 BOT_TOKEN=("7748376075:AAFR7Rnsace57B08Of1ZFpNsg-cF3kP_c7Y")
 
-OWNER_USERNAME=("@rajaraj_04")
+  #Enter Your telegram username here without @
+OWNER_USERNAME=("@RAHUL_DDOS_B")
 
+  #Enter your admin id here Get it from @missRose_bot by typing /info
 ADMIN_IDS=("1661744209")
-
-
 
 
 
