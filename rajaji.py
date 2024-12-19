7697cc21d6dde02e52865f7a9fa57004c180c3bd
Hejwkwkwkk <pulkittravis@gmail.com>
2024-12-19 23:23:10 +0530

    Update rajaji.py

--- rajaji.py
+++ rajaji.py
@@ -2,13 +2,13 @@
 #SCRIPT BY TU ADMIN NHI HAI TU CHUTIYA HAI
   
 #Enter Your Bot Token here get it from @botfarher
-BOT_TOKEN=("7748376075:AAFR7Rnsace57B08Of1ZFpNsg-cF3kP_c7Y")
+BOT_TOKEN=("7550636624:AAHw612XCydGXKYSpXavCsLUBA0tZsLPX7U")
 
   #Enter Your telegram username here without @
-OWNER_USERNAME=("@RAHUL_DDOS_B")
+OWNER_USERNAME=("@rajaraj_04")
 
   #Enter your admin id here Get it from @missRose_bot by typing /info
-ADMIN_IDS=("1661744209")
+ADMIN_IDS=("5712168482")
 
 
 
